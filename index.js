6af40dba74e53f90130ae887bce2bdf04f26fbfa
OfentseXI <dofentse69@gmail.com>
2024-03-28 09:42:17 +0200

    Song Additions

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const songs = [
     { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
+    { title: "Depressed Jim Carrey/Clementine", artist: "Baldemar", genre: "R&B" },
+    { title: "Fool for you", artist: "Kaidraroo", genre: "Pop" },
+    { title: "Valentine's Curse", artist: "Banes World", genre: "R&B" },
+    { title: "Running up that hill", artist: "Kate Bush", genre: "Pop" },
+    { title: "Cocaine Sunday", artist: "Eyedress", genre: "Rock" },
     // Feel free to add even more songs
 ];
 
-
-/
