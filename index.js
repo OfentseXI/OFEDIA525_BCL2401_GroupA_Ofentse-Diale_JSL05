35f0cb4ff6b101f53ddeddf1b84825d3a7301fd4
OfentseXI <dofentse69@gmail.com>
2024-03-28 09:28:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Array of song objects. Add at least 5 songs with title, artist, and genre properties.
+const songs = [
+    { title: "Hooked on a Feeling", artist: "Blue Swede", genre: "Pop" },
+    { title: "Moonage Daydream", artist: "David Bowie", genre: "Rock" },
+    { title: "I Want You Back", artist: "The Jackson 5", genre: "Pop" },
+    { title: "Spirit in the Sky", artist: "Norman Greenbaum", genre: "Rock" },
+    { title: "Cherry Bomb", artist: "The Runaways", genre: "Rock" },
+    { title: "Escape (The Piña Colada Song)", artist: "Rupert Holmes", genre: "Pop" },
+    { title: "O-O-H Child", artist: "The Five Stairsteps", genre: "R&B" },
+    { title: "Ain't No Mountain High Enough", artist: "Marvin Gaye & Tammi Terrell", genre: "R&B" },
+    { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
+    { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
+    { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
+    // Feel free to add even more songs
+];
+
+
+/
